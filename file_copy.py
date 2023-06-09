ec7a94152c7356b8f8fbc573e4fe1299bec27c86
Lugaziva <kelvin.ligaziva@roamtech.com>
2023-06-09 12:05:28 +0300

    update file_copy.py add logic to make trail of not found files

--- file_copy.py
+++ file_copy.py
@@ -6,6 +6,8 @@ import sys
 def copy_files(csv_file, source_folder, destination_folder):
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
+    not_found_file = 'not_found_files.csv'
+    not_found_files = []
 
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
@@ -22,8 +24,15 @@ def copy_files(csv_file, source_folder, destination_folder):
                 print(f"Successfully copied {file_name}")
             except FileNotFoundError:
                 print(f"File not found: {file_name}")
+                not_found_files.append(file_name)
             except Exception as e:
                 print(f"Error occurred while copying {file_name}: {str(e)}")
+    if not_found_files:
+        with open(not_found_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['File Name'])
+            writer.writerows([[file_name] for file_name in not_found_files])
+        print(f"\nList of not found files is saved in {not_found_file}")
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
